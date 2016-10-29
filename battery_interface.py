1ceb06b919465937701d6349a73d8a11b9dba8cb
JKWall <JhonRA55@hotmail.com>
2016-10-29 17:26:54 -0500

    Update

--- battery_interface.py
+++ battery_interface.py
@@ -7,30 +7,36 @@ from std_msgs.msg import String
 from std_msgs.msg import Float64
 
 
-globvar = Float64()
+globvar = 13.0 
 
-def talker():
+
+def talker(bateryAux, cont):
      pub = rospy.Publisher('mybattery', Float64, queue_size=10)
      rospy.init_node('oodometry', anonymous=True) #make node 
      rospy.Subscriber('/RosAria/battery_voltage',Float64,batteryCb)
      rate = rospy.Rate(10)
      while not rospy.is_shutdown():
-          if globvar < 11.0:
-               hello = "La cosa va bien"
-               pub.publish(globvar)
-               rate.sleep()
+          bateryAux += globvar;
+          cont += 1
+          if cont == 10:
+               batery = bateryAux/10
+               cont = 0
+               bateryAux = 0
+               if batery < 11.0:
+                    pub.publish(Float64(batery))
+          rate.sleep()
 
 def batteryCb(msg):
-     #rospy.loginfo("X is %s", msg)
-     if msg.data < 11.0:
-          global globvar
-          globvar = msg.data
+     global globvar
+     globvar = msg.data
          #print "position x" + msg.pose.pose.position.x
           #pub.publish(msg)
 
 if __name__ == "__main__":
+     bateryAux = 0.0
+     cont = 0
      try:
-          talker()
+          talker(bateryAux, cont)
      except rospy.ROSInterruptException:
           pass
     
